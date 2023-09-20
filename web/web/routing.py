ab5fb3ee91fec4f184e34eb74e2f42c286c94251
Narek <narek.davtyan2002@mail.ru>
2023-09-20 22:46:28 +0300

    chat

---
+++ web/web/routing.py
@@ -0,0 +1,11 @@
+# web/routing.py
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import re_path
+from chat.consumers import ChatConsumer
+
+application = ProtocolTypeRouter({
+    "websocket": URLRouter([
+        re_path(r"ws/chat/(?P<room_name>\w+)/$", ChatConsumer.as_asgi()),
+    ]),
+})
